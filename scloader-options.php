c35cf36bd09d237596fe95659e8c17f1590fb9a5
shoyebzz <shoyeb.abir21@gmail.com>
2016-09-05 02:34:17 +0600

    fisrt commit

---
+++ scloader-options.php
@@ -0,0 +1,66 @@
+<?php
+
+//common slag scloader_
+
+// add option page to the admin menu
+function scloader_option_admin_menu() {
+    add_options_page( 'SC loader Options', 'Loader Options', 'manage_options', 'scloader_options.php', 'scloader_main_callbk_function' );
+    
+    //Activating custom setting
+    add_action('admin_init', 'scloader_custom_settings');
+    
+}
+add_action( 'admin_menu', 'scloader_option_admin_menu' );
+
+
+// custom setting callback function
+function scloader_custom_settings(){
+    
+    //registering settings
+    register_setting( 'scloader-settings-group', 'background_color');
+    
+    //settings sections 
+    add_settings_section( 'scloader-genarel-section', 'Color Section', 'genarel_section_callback', 'scloader_options.php' );
+    
+    //adding setting fields
+    add_settings_field( 'scloader-background-color', 'Background Color', 'background_color_field_callback', 'scloader_options.php', 'scloader-genarel-section' );
+    
+}
+
+
+// section callbacks
+function genarel_section_callback(){
+    echo 'customize all colors';
+}
+
+// option fields callback
+function background_color_field_callback(){
+    $backgroundColor = esc_attr( get_option( 'background_color' ) );
+    echo '<input type="color" name="background_color" value="'.$backgroundColor.'"/>';
+}
+
+
+
+// callback function
+function scloader_main_callbk_function(){  ?>
+   
+   
+    <div class="wrap">
+        <h1 class="title">SC Loader Options</h1>
+        <?php settings_errors('scloader_options.php'); ?>
+        <form method="post" action="options.php">
+            <?php settings_fields( 'scloader-settings-group' ); ?>
+            <?php do_settings_sections( 'scloader_options.php' ); ?>
+            <?php submit_button(); ?>
+        </form>
+        
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+<?php }
---
+++ scloader-plugin.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: SC Loader
+Plugin URI: http://stylishcreativity.com
+Author: Mashiur Rahman
+Author URI: http://stylishcreativity.com
+*/
+
+// include admin option
+require_once('scloader-options.php');
+
+// load all style and scripts
+function scloader_styles_and_script() {
+    wp_enqueue_script( 'jquery' );
+    
+    // Register the style for plugin:
+    wp_register_style( 'scloader-style', plugins_url( '/css/scloader-style.css', __FILE__ ), array(), 'all' );
+    // For either a plugin or a theme, you can then enqueue the style:
+    wp_enqueue_style( 'scloader-style' );
+    
+    // Register the script for plugin:
+    wp_register_script('scloader_main_script', plugins_url( '/js/scloader-plugin.js', __FILE__ ), array('jquery'),'1.0', true);
+    // For either a plugin or a theme, you can then enqueue the script:
+    wp_enqueue_script('scloader_main_script');
+}
+add_action('wp_enqueue_scripts', 'scloader_styles_and_script' );
+
+
+
+
+
+/******************** inline styleing ******************/
+function stycwplogin_theme_styles_inline() {?>
+    
+    <style type="text/css">
+
+        
+    </style>
+<?php } 
+add_action( 'login_enqueue_scripts', 'stycwplogin_theme_styles_inline' );
+
+
+//adding the loader html in website
+function custom_content_after_body_open_tag() {
+    ?>
+    
+    <div id="scloader_main">
+        <img src="<?php echo plugins_url( '/loaders/flat/Preloader_1.gif', __FILE__ ); ?>">
+    </div>
+
+    <?php
+
+}
+
+add_action('wp_footer', 'custom_content_after_body_open_tag');
